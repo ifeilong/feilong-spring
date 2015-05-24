0cd3420bf91feb9e8a4d5f191c841ed8df63c1c8
feilong <venusdrogon@163.com>
2015-05-25 02:16:50 +0800

    init

---
+++ feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/ModelAndViewUtil.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2008 feilong (venusdrogon@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.spring.web.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author <a href="mailto:venusdrogon@163.com">feilong</a>
+ * @version 1.1.2 2015年5月16日 上午9:13:29
+ * @since 1.1.2
+ */
+public class ModelAndViewUtil{
+
+    private static final Logger log = LoggerFactory.getLogger(ModelAndViewUtil.class);
+
+    //    public final static Map<String, String> getModelAndViewMapInfoForLog(ModelAndView modelAndView){
+    //        Map<String, String> object = new LinkedHashMap<String, String>();
+    //        String viewName = modelAndView.getViewName();
+    //        Map<String, Object> model = modelAndView.getModel();
+    //        log.info("viewName:{}", viewName);
+    //        log.info("model attribute:{}", model.get("isSuccess").toString());
+    //        object.put("", "");
+    //        object.put("", "");
+    //        object.put("", "");
+    //        object.put("", "");
+    //
+    //        return object;
+    //    }
+}
