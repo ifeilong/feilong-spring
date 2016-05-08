d663943d8c6bf9f233835f17fbb6f0ca0e0355b5
feilong <venusdrogon@163.com>
2016-05-08 23:01:35 +0800

    update javadoc

--- feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/FlashMapUtil.java
+++ feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/FlashMapUtil.java
@@ -43,6 +43,13 @@ import org.springframework.web.servlet.view.RedirectView;
  */
 public class FlashMapUtil{
 
+    /** Don't let anyone instantiate this class. */
+    private FlashMapUtil(){
+        //AssertionError不是必须的. 但它可以避免不小心在类的内部调用构造器. 保证该类在任何情况下都不会被实例化.
+        //see 《Effective Java》 2nd
+        throw new AssertionError("No " + getClass().getName() + " instances for you!");
+    }
+
     public static FlashMap put(String name,String value,String targetRequestPath,HttpServletRequest request){
         FlashMap flashMap = RequestContextUtils.getOutputFlashMap(request);
         flashMap.put(name, value);
