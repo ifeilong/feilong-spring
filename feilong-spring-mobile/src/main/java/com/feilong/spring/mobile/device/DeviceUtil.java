b4254560472da39b76a34ee54996137164a57aa4
feilong <venusdrogon@163.com>
2016-05-31 16:34:00 +0800

    add feilong-spring-mobile

---
+++ feilong-spring-mobile/src/main/java/com/feilong/spring/mobile/device/DeviceUtil.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.spring.mobile.device;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.DeviceResolver;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.DeviceResolverRequestFilter;
+import org.springframework.mobile.device.LiteDeviceResolver;
+
+/**
+ * The Class DeviceUtil.
+ *
+ * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ * @see org.springframework.mobile.device.DeviceUtils
+ * @since 1.5.7
+ */
+public class DeviceUtil{
+
+    /**
+     * The Constant DEVICE_RESOLVER.
+     * 
+     * @see org.springframework.mobile.device.LiteDeviceResolver#LiteDeviceResolver()
+     */
+    private static final DeviceResolver DEVICE_RESOLVER = new LiteDeviceResolver();
+
+    /**
+     * 通过 {@link HttpServletRequest} 解析获得 {@link Device},以便区分 {@link Device#isMobile()},{@link Device#isNormal()},{@link Device#isTablet()}.
+     * 
+     * <p>
+     * 此方法适用于没有配置 {@link DeviceResolverHandlerInterceptor}或者 {@link DeviceResolverRequestFilter}或者
+     * {@link DeviceHandlerMethodArgumentResolver} 的场景,如果你所在项目的环境配置了上述的参数,可以直接使用
+     * {@link org.springframework.mobile.device.DeviceUtils#getCurrentDevice(HttpServletRequest)}
+     * </p>
+     * 
+     * <p>
+     * 以往的实现方式是通过配置 {@link DeviceResolverHandlerInterceptor}或者 {@link DeviceResolverRequestFilter}或者
+     * {@link DeviceHandlerMethodArgumentResolver},<br>
+     * 目前的实现方式是:按需加载,当有需要的时候调用该方法
+     * </p>
+     * 
+     * <p>
+     * 目前 spring mobile的默认实现是 {@link LiteDeviceResolver},参见
+     * {@link org.springframework.mobile.device.DeviceResolverHandlerInterceptor#DeviceResolverHandlerInterceptor()}
+     * </p>
+     *
+     * @param request
+     *            request
+     * @return {@link Device}
+     * @see org.springframework.mobile.device.DeviceResolverHandlerInterceptor#DeviceResolverHandlerInterceptor()
+     * @see org.springframework.mobile.device.LiteDeviceResolver#resolveDevice(HttpServletRequest)
+     * @see org.springframework.mobile.device.DeviceUtils#getCurrentDevice(HttpServletRequest)
+     */
+    public static Device getDevice(HttpServletRequest request){
+        return DEVICE_RESOLVER.resolveDevice(request);
+    }
+}
---
+++ feilong-spring-mobile/src/main/java/com/feilong/spring/mobile/device/view/TilesSupportLiteDeviceDelegatingViewResolver.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.spring.mobile.device.view;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceUtils;
+import org.springframework.mobile.device.site.SitePreference;
+import org.springframework.mobile.device.site.SitePreferenceUtils;
+import org.springframework.mobile.device.util.ResolverUtils;
+import org.springframework.mobile.device.view.LiteDeviceDelegatingViewResolver;
+import org.springframework.util.Assert;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ViewResolver;
+
+/**
+ * Integration of Tiles with Spring Moblie
+ *
+ * Dynamically loads the the location of JSP using Tiles Expression Language.
+ * 
+ * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ * @since 1.5.7
+ */
+public class TilesSupportLiteDeviceDelegatingViewResolver extends LiteDeviceDelegatingViewResolver{
+
+    /** The device type. */
+    private String deviceType;
+
+    /**
+     * Instantiates a new tiles support lite device delegating view resolver.
+     *
+     * @param resolver
+     *            the resolver
+     */
+    public TilesSupportLiteDeviceDelegatingViewResolver(ViewResolver resolver){
+        super(resolver);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.springframework.mobile.device.view.LiteDeviceDelegatingViewResolver#getDeviceViewNameInternal(java.lang.String)
+     */
+    @Override
+    protected String getDeviceViewNameInternal(String viewName){
+        RequestAttributes attrs = RequestContextHolder.getRequestAttributes();
+        Assert.isInstanceOf(ServletRequestAttributes.class, attrs);
+        HttpServletRequest request = ((ServletRequestAttributes) attrs).getRequest();
+        Device device = DeviceUtils.getCurrentDevice(request);
+        SitePreference sitePreference = SitePreferenceUtils.getCurrentSitePreference(request);
+
+        String resolvedViewName = viewName;
+
+        String attributeValue = null;
+        if (ResolverUtils.isNormal(device, sitePreference)){
+            resolvedViewName = getNormalPrefix() + viewName + getNormalSuffix();
+            attributeValue = getNormalPrefix();
+        }else if (ResolverUtils.isMobile(device, sitePreference)){
+            resolvedViewName = getMobilePrefix() + viewName + getMobileSuffix();
+            attributeValue = getMobilePrefix();
+        }else if (ResolverUtils.isTablet(device, sitePreference)){
+            resolvedViewName = getTabletPrefix() + viewName + getTabletSuffix();
+            attributeValue = getTabletPrefix();
+        }
+        request.setAttribute(deviceType, attributeValue);
+        // MOBILE-63 "redirect:/" and "forward:/" can result in the view name containing multiple trailing slashes
+        return stripTrailingSlash(resolvedViewName);
+    }
+
+    /**
+     * Strip trailing slash.
+     *
+     * @param viewName
+     *            the view name
+     * @return the string
+     */
+    private String stripTrailingSlash(String viewName){
+        if (viewName.endsWith("//")){
+            return viewName.substring(0, viewName.length() - 1);
+        }
+        return viewName;
+    }
+
+    /**
+     * Sets the device type.
+     *
+     * @param deviceType
+     *            the deviceType to set
+     */
+    public void setDeviceType(String deviceType){
+        this.deviceType = deviceType;
+    }
+}
