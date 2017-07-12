f23c6a8a6e1880fa66d2e91bd444598f3151aa26
feilong <venusdrogon@163.com>
2017-07-12 23:47:18 +0800

    ClientCacheInterceptor isDefaultNoCache fix #13

--- feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/interceptor/clientcache/ClientCacheInterceptor.java
+++ feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/interceptor/clientcache/ClientCacheInterceptor.java
@@ -16,6 +16,10 @@
 package com.feilong.spring.web.servlet.interceptor.clientcache;
 
 import static com.feilong.core.CharsetType.UTF8;
+import static com.feilong.servlet.http.HttpHeaders.CACHE_CONTROL;
+import static com.feilong.servlet.http.RequestUtil.getRequestFullURL;
+import static com.feilong.spring.web.method.HandlerMethodUtil.getDeclaringClassSimpleName;
+import static com.feilong.spring.web.method.HandlerMethodUtil.getHandlerMethodName;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,9 +31,7 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.feilong.servlet.http.HttpHeaders;
-import com.feilong.servlet.http.RequestUtil;
 import com.feilong.servlet.http.ResponseUtil;
-import com.feilong.spring.web.method.HandlerMethodUtil;
 import com.feilong.spring.web.servlet.interceptor.AbstractHandlerInterceptorAdapter;
 
 /**
@@ -96,7 +98,18 @@ import com.feilong.spring.web.servlet.interceptor.AbstractHandlerInterceptorAdap
 public class ClientCacheInterceptor extends AbstractHandlerInterceptorAdapter{
 
     /** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(ClientCacheInterceptor.class);
+    private static final Logger LOGGER           = LoggerFactory.getLogger(ClientCacheInterceptor.class);
+
+    //---------------------------------------------------------------
+
+    /**
+     * 是否默认不缓存.
+     * 
+     * @since 1.10.4
+     */
+    private boolean             isDefaultNoCache = false;
+
+    //---------------------------------------------------------------
 
     /*
      * (non-Javadoc)
@@ -108,44 +121,98 @@ public class ClientCacheInterceptor extends AbstractHandlerInterceptorAdapter{
     public void postHandle(HttpServletRequest request,HttpServletResponse response,Object handler,ModelAndView modelAndView)
                     throws Exception{
         if (!(handler instanceof HandlerMethod)){
-            LOGGER.warn(
-                            "request info:[{}],not [HandlerMethod],handler is [{}],What ghost~~,",
-                            RequestUtil.getRequestFullURL(request, UTF8),
-                            handler.getClass().getName());
+
+            if (LOGGER.isWarnEnabled()){
+                LOGGER.warn(
+                                "request info:[{}],not [HandlerMethod],handler is [{}],What ghost~~,",
+                                getRequestFullURL(request, UTF8),
+                                handler.getClass().getName());
+            }
             return;
         }
 
         //---------------------------------------------------------------
 
         HandlerMethod handlerMethod = (HandlerMethod) handler;
-        //如果没有标识{@link ClientCache},那么自动通过拦截器,不进行任何处理
+
+        //如果没有标识{@link ClientCache}
         ClientCache clientCache = handlerMethod.getMethodAnnotation(ClientCache.class);
+
+        //---------------------------------------------------------------
         if (clientCache == null){
+            //默认 nocache
+            if (isDefaultNoCache){
+                setNoCacheAndLog(response, handlerMethod, "isDefaultNoCache:[" + isDefaultNoCache + "]");
+                return;
+            }
+            //那么自动通过拦截器,不进行任何处理
             return;
         }
 
+        //---------------------------------------------------------------
         int value = clientCache.value();
 
-        //---------------------------------------------------------------
-
-        //如果标识的{@link ClientCache},{@link ClientCache#value()} <=0,那么标识不设置缓存,参见 {@link ResponseUtil#setNoCacheHeader(HttpServletResponse)}
+        //如果标识的{@link ClientCache}且{@link ClientCache#value()} <=0,那么标识不设置缓存
         if (value <= 0){
-            ResponseUtil.setNoCacheHeader(response);
-            LOGGER.debug(
-                            "[{}.{}()],setNoCacheHeader",
-                            HandlerMethodUtil.getDeclaringClassSimpleName(handlerMethod),
-                            HandlerMethodUtil.getHandlerMethodName(handlerMethod));
+            setNoCacheAndLog(response, handlerMethod, "value:[" + value + "] <=0");
         }
         //否则,会调用 {@link HttpServletResponse#setHeader(String, String)},添加 {@link HttpHeaders#CACHE_CONTROL}头,value值为 {@code "max-age=" + value}
         else{
-            ResponseUtil.setCacheHeader(response, value);
-
-            LOGGER.debug(
-                            "[{}.{}()],set response setHeader:[{}],value is :[{}]",
-                            HandlerMethodUtil.getDeclaringClassSimpleName(handlerMethod),
-                            HandlerMethodUtil.getHandlerMethodName(handlerMethod),
-                            HttpHeaders.CACHE_CONTROL,
-                            value);
+            setCacheAndLog(response, handlerMethod, value);
         }
+    }
+
+    /**
+     * 设置 cache and log.
+     *
+     * @param response
+     *            the response
+     * @param handlerMethod
+     *            the handler method
+     * @param value
+     *            the value
+     * @since 1.10.4
+     */
+    private static void setCacheAndLog(HttpServletResponse response,HandlerMethod handlerMethod,int value){
+        ResponseUtil.setCacheHeader(response, value);
+
+        if (LOGGER.isDebugEnabled()){
+            String pattern = "[{}.{}()],set response setHeader:[{}],value is :[{}]";
+            LOGGER.debug(pattern, getDeclaringClassSimpleName(handlerMethod), getHandlerMethodName(handlerMethod), CACHE_CONTROL, value);
+        }
+    }
+
+    /**
+     * 设置 no cache and log.
+     *
+     * @param response
+     *            the response
+     * @param handlerMethod
+     *            the handler method
+     * @since 1.10.4
+     */
+    private static void setNoCacheAndLog(HttpServletResponse response,HandlerMethod handlerMethod,String reason){
+        ResponseUtil.setNoCacheHeader(response);
+
+        if (LOGGER.isDebugEnabled()){
+            LOGGER.debug(
+                            "[{}.{}()],setNoCacheHeader,cause by: [{}]",
+                            getDeclaringClassSimpleName(handlerMethod),
+                            getHandlerMethodName(handlerMethod),
+                            reason);
+        }
+    }
+
+    //---------------------------------------------------------------
+
+    /**
+     * 设置 是否默认不缓存.
+     *
+     * @param isDefaultNoCache
+     *            the isDefaultNoCache to set
+     * @since 1.10.4
+     */
+    public void setIsDefaultNoCache(boolean isDefaultNoCache){
+        this.isDefaultNoCache = isDefaultNoCache;
     }
 }
