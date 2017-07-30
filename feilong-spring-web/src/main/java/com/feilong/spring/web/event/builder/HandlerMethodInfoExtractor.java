2d6ef13aeb6f25340f065eb41c13cb50540cd83a
feilong <venusdrogon@163.com>
2017-07-31 03:30:35 +0800

    write private HandlerMethodInfoExtractor

--- feilong-spring-web/src/main/java/com/feilong/spring/web/event/builder/HandlerMethodInfoExtractor.java
+++ feilong-spring-web/src/main/java/com/feilong/spring/web/event/builder/HandlerMethodInfoExtractor.java
@@ -38,6 +38,13 @@ import com.feilong.core.lang.annotation.AnnotationToStringBuilder;
  */
 public class HandlerMethodInfoExtractor{
 
+    /** Don't let anyone instantiate this class. */
+    private HandlerMethodInfoExtractor(){
+        //AssertionError不是必须的. 但它可以避免不小心在类的内部调用构造器. 保证该类在任何情况下都不会被实例化.
+        //see 《Effective Java》 2nd
+        throw new AssertionError("No " + getClass().getName() + " instances for you!");
+    }
+
     /**
      * 提取.
      *
