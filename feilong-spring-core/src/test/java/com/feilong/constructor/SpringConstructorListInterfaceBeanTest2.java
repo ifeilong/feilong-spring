87448fef73fbd020392301c39f79b8caf0a11620
feilong <venusdrogon@163.com>
2018-09-17 12:36:14 +0800

    add test

---
+++ feilong-spring-core/src/test/java/com/feilong/constructor/SpringConstructorListInterfaceBeanTest2.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.constructor;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItemInArray;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.feilong.entity.ConstructorListInterfaceBeanEntity;
+import com.feilong.entity.InterfaceUser;
+
+@ContextConfiguration(locations = { "classpath:spring-DI-constructor-list-interface-bean.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringConstructorListInterfaceBeanTest2{
+
+    @Autowired
+    private ConstructorListInterfaceBeanEntity constructorListInterfaceBeanEntity;
+
+    //---------------------------------------------------------------
+
+    @Autowired
+    @Qualifier("user1")
+    private InterfaceUser                      user1;
+
+    @Autowired
+    @Qualifier("user2")
+    private InterfaceUser                      user2;
+
+    //---------------------------------------------------------------
+    @Test
+    public void test(){
+        InterfaceUser[] users = constructorListInterfaceBeanEntity.getUsers();
+        assertThat(users, allOf(hasItemInArray(user1), hasItemInArray(user2)));
+    }
+}
---
+++ feilong-spring-core/src/test/java/com/feilong/constructor/SpringConstructorListStringTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.constructor;
+
+import static com.feilong.core.bean.ConvertUtil.toList;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.feilong.entity.ConstructorListStringEntity;
+
+@ContextConfiguration(locations = { "classpath:spring-DI-constructor-list-string.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringConstructorListStringTest{
+
+    @Autowired
+    private ConstructorListStringEntity constructorListStringEntity;
+
+    @Test
+    public void test(){
+        String[] names = constructorListStringEntity.getNames();
+        assertThat(
+                        toList(names),
+                        allOf(
+                                        hasItem("http://10.12.38.41:8080/solr4/adidas_pro"),
+                                        hasItem("http://10.12.38.42:8080/solr4/adidas_pro"),
+                                        hasItem("http://10.12.38.43:8080/solr4/adidas_pro"),
+                                        hasItem("http://10.12.38.43:8080/solr4/adidas_pro")));
+    }
+}
