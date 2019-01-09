0a4e06a36d627d829875992ca5f28b219e0eac44
feilong <venusdrogon@163.com>
2019-01-09 15:49:29 +0800

    PrintWriterView 加上日志 fix #170

--- feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/view/printwriter/PrintWriterView.java
+++ feilong-spring-web/src/main/java/com/feilong/spring/web/servlet/view/printwriter/PrintWriterView.java
@@ -20,20 +20,31 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.view.AbstractView;
 
 import com.feilong.servlet.http.ResponseUtil;
 
 /**
- * The Class PrintWriterView.
+ * 直接输出的 view.
+ * 
+ * <p>
+ * 实现原理,调用 {@link com.feilong.servlet.http.ResponseUtil#writeText(HttpServletResponse, Object)} 方法
+ * </p>
  *
  * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
  * @since 4.0.3
  */
 public class PrintWriterView extends AbstractView{
 
-    /** The content. */
-    private String content;
+    /** The Constant log. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrintWriterView.class);
+
+    //---------------------------------------------------------------
+
+    /** 待输出的内容. */
+    private String              content;
 
     //---------------------------------------------------------------
 
@@ -59,26 +70,31 @@ public class PrintWriterView extends AbstractView{
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model,HttpServletRequest request,HttpServletResponse response)
                     throws Exception{
-
+        //since 4.0.4 add log
+        if (LOGGER.isDebugEnabled()){
+            LOGGER.debug("will write:[{}]", content);
+        }
+        //---------------------------------------------------------------
         ResponseUtil.writeText(response, content);
     }
 
     //---------------------------------------------------------------
 
     /**
-     * Gets the content.
+     * 获得 待输出的内容.
      *
-     * @return the content
+     * @return the 待输出的内容
      */
+
     public String getContent(){
         return content;
     }
 
     /**
-     * Sets the content.
+     * 设置 待输出的内容.
      *
      * @param content
-     *            the content to set
+     *            the new 待输出的内容
      */
     public void setContent(String content){
         this.content = content;
