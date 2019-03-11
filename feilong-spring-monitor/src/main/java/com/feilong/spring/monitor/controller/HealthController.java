14ef506cd45beaa41afef94048c39c35192093cd
feilong <venusdrogon@163.com>
2019-03-11 16:05:26 +0800

    新建 feilong-spring-monitor 项目 fix #174

---
+++ feilong-spring-monitor/src/main/java/com/feilong/spring/monitor/controller/HealthController.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.spring.monitor.controller;
+
+import static com.feilong.core.bean.ConvertUtil.toMap;
+
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.feilong.core.util.ResourceBundleUtil;
+import com.feilong.json.jsonlib.JsonUtil;
+
+/**
+ * 用来给运维平台健康扫描用的.
+ * 
+ * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ * @since 4.0.4
+ */
+@Controller
+public class HealthController{
+
+    /**
+     * 和后端保持 url 一致.
+     *
+     * @return the string
+     */
+    @RequestMapping("/customize_health")
+    @ResponseBody
+    public String invoke(){
+        return JsonUtil.format(buildMapData(), 0, 0);
+    }
+
+    /**
+     * Builds the map data.
+     *
+     * @return the map< string,? extends object>
+     */
+    private static Map<String, ? extends Object> buildMapData(){
+        //git.properties
+        String name = "git";
+        String key = "git.commit.id";
+
+        //---------------------------------------------------------------
+
+        try{
+            ResourceBundle resourceBundle = ResourceBundleUtil.getResourceBundle(name);
+            String commitId = ResourceBundleUtil.getValue(resourceBundle, key);
+            return toMap("status", 200, "codeVersion", commitId);
+        }catch (Exception e){
+            return toMap("status", 501, "codeVersion", "Exception " + e.getMessage());
+        }
+    }
+
+}
---
+++ feilong-spring-monitor/src/test/java/com/feilong/controller/BaseControllerTest.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import com.feilong.core.DefaultRuntimeException;
+import com.feilong.json.jsonlib.JsonUtil;
+import com.feilong.spring.context.ApplicationContextUtil;
+
+//Spring-mvc-test does not read the web.xml file, but you can configure the filters this way:
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:springmvc/springmvc-servlet.xml" })
+//测试环境使用,用来表示测试环境使用的ApplicationContext将是WebApplicationContext类型的;value指定web应用的根;
+@WebAppConfiguration(value = "src/main/webapp")
+public class BaseControllerTest{
+
+    /** The Constant LOGGER. */
+    private static final Logger       LOGGER = LoggerFactory.getLogger(BaseControllerTest.class);
+
+    //---------------------------------------------------------------
+
+    /** The mock mvc. */
+    protected MockMvc                 mockMvc;
+
+    //---------------------------------------------------------------
+
+    /** The request. */
+    protected MockHttpServletRequest  request;
+
+    /** The response. */
+    protected MockHttpServletResponse response;
+
+    //---------------------------------------------------------------
+
+    /** The context. */
+    @Autowired
+    protected WebApplicationContext   webApplicationContext;
+
+    //---------------------------------------------------------------
+
+    @Before
+    public void before(){
+        DefaultMockMvcBuilder defaultMockMvcBuilder = MockMvcBuilders.webAppContextSetup(webApplicationContext);
+        this.mockMvc = defaultMockMvcBuilder.build();
+    }
+
+    //---------------------------------------------------------------
+
+    @Before
+    public void setUp() throws Exception{
+        this.request = new MockHttpServletRequest();
+        this.response = new MockHttpServletResponse();
+    }
+
+    //---------------------------------------------------------------
+    /**
+     * Perform.
+     *
+     * @param urlTemplate
+     *            the url template
+     * @param urlVariables
+     *            the url variables
+     * @since 1.0.9
+     */
+    protected void perform(String urlTemplate,Object...urlVariables){
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(urlTemplate, urlVariables);
+        ResultHandler resultHandler = MockMvcResultHandlers.print();
+        try{
+            ResultActions resultActions = mockMvc.perform(requestBuilder);
+            ResultActions andDo = resultActions.andDo(resultHandler);
+        }catch (Exception e){
+            throw new DefaultRuntimeException(e);
+        }
+    }
+
+    protected ModelAndView handle(HttpServletRequest request){
+        LOGGER.debug(JsonUtil.format(ApplicationContextUtil.getApplicationContextInfoMapForLog(webApplicationContext)));
+
+        HandlerMapping handlerMapping = webApplicationContext.getBean(HandlerMapping.class);
+
+        try{
+            HandlerExecutionChain handler = handlerMapping.getHandler(request);
+
+            // assertNotNull("No handler found for request, check you request mapping", handler);
+            Object controller = handler.getHandler();
+
+            // if you want to override any injected attributes do it here
+
+            //Deprecated. in Spring 3.2 in favor of RequestMappingHandlerAdapter
+            //HandlerAdapter handlerAdapter = new AnnotationMethodHandlerAdapter();
+
+            HandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, controller);
+
+            LOGGER.debug(JsonUtil.format(modelAndView));
+            return modelAndView;
+        }catch (Exception e){
+            throw new DefaultRuntimeException(e);
+        }
+    }
+}
