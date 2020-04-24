35822bdac45d49e105048485e3ed5994ac1049af
feilong <venusdrogon@163.com>
2020-04-24 09:32:58 +0800

    :pencil: update javadoc

---
+++ feilong-spring-extension/src/test/java/com/feilong/spring/web/event/builder/FilterBeanToMapBuilder.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2008 feilong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.feilong.spring.web.event.builder;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.feilong.spring.event.builder.BeanToMapBuilder;
+import com.feilong.spring.event.builder.SimpleBeanToMapBuilder;
+
+/**
+ * {@link HandlerExceptionResolver} 信息提取.
+ * 
+ * <h3>作用:</h3>
+ * 
+ * <blockquote>
+ * 
+ * <p>
+ * 可以在日志文件或者控制台输出如下信息:
+ * </p>
+ * 
+ * <pre class="code">
+14:28:16 INFO  (ContextRefreshedBeanLoggingEventListener.java:134) doLog() - [interface org.springframework.web.servlet.HandlerExceptionResolver] list size:[5], Info:
+beanName                                                                                  name                                                                                    order      
+----------------------------------------------------------------------------------------- --------------------------------------------------------------------------------------- ---------- 
+defaultHandlerExceptionResolver                                                           org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver             2147483647 
+org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver#0          org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver          1          
+org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver#0 org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver 0          
+org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver#0             org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver             2          
+simpleMappingHandlerMethodExceptionResolver                                               com.feilong.spring.web.servlet.handler.SimpleMappingHandlerMethodExceptionResolver      1
+ * 
+ * </pre>
+ * 
+ * </blockquote>
+ * 
+ * <h3>参考配置:</h3>
+ * 
+ * <blockquote>
+ * 
+ * <pre class="code">
+{@code 
+    <!-- 启动的时候,显示 HandlerExceptionResolver 信息 -->
+    <bean id="contextRefreshedHandlerExceptionResolverEventListener" class=
+"com.feilong.spring.web.event.ContextRefreshedBeanLoggingEventListener">
+        <property name="beanClass" value="org.springframework.web.servlet.HandlerExceptionResolver" />
+        <property name="beanToMapBuilder">
+            <bean class="com.feilong.spring.web.event.builder.HandlerExceptionResolverBeanToMapBuilder" />
+        </property>
+    </bean>
+}
+ * </pre>
+ * 
+ * </blockquote>
+ * 
+ * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ * @since 1.12.7
+ */
+public class FilterBeanToMapBuilder extends SimpleBeanToMapBuilder<Filter>{
+
+    /** Static instance. */
+    // the static instance works for all types
+    public static final BeanToMapBuilder<Filter> INSTANCE = new FilterBeanToMapBuilder();
+
+}
---
+++ feilong-spring-extension/src/test/java/com/feilong/spring/web/servlet/interceptor/loginsessionbind/ClientInfo.java
@@ -0,0 +1,100 @@
+package com.feilong.spring.web.servlet.interceptor.loginsessionbind;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 保存用户请求客户端信息.
+ *
+ * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ * @since 5.3.2.18
+ */
+public class ClientInfo implements Serializable{
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 288232184048495608L;
+
+    /** ua. */
+    private String            userAgent;
+
+    /** 客户端识别码. */
+    private String            fingerPrint;
+
+    //---------------------------------------------------------------
+
+    /**
+     * Instantiates a new client info.
+     */
+    public ClientInfo(){
+        super();
+    }
+
+    /**
+     * Instantiates a new client info.
+     *
+     * @param userAgent
+     *            the user agent
+     * @param fingerPrint
+     *            the finger print
+     */
+    public ClientInfo(String userAgent, String fingerPrint){
+        super();
+        this.userAgent = userAgent;
+        this.fingerPrint = fingerPrint;
+    }
+
+    //---------------------------------------------------------------
+
+    /**
+     * 获得 ua.
+     *
+     * @return the userAgent
+     */
+    public String getUserAgent(){
+        return userAgent;
+    }
+
+    /**
+     * 设置 ua.
+     *
+     * @param userAgent
+     *            the userAgent to set
+     */
+    public void setUserAgent(String userAgent){
+        this.userAgent = userAgent;
+    }
+
+    /**
+     * 获得 客户端识别码.
+     *
+     * @return the fingerPrint
+     */
+    public String getFingerPrint(){
+        return fingerPrint;
+    }
+
+    /**
+     * 设置 客户端识别码.
+     *
+     * @param fingerPrint
+     *            the fingerPrint to set
+     */
+    public void setFingerPrint(String fingerPrint){
+        this.fingerPrint = fingerPrint;
+    }
+
+    //---------------------------------------------------------------
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString(){
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
